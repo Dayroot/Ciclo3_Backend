9934b136cdbddadd682f54d3c8fc562bd20c8efc
Dayroot <dallancaceresp@gmail.com>
2021-10-05 20:55:56 -0500

    Modifique el serializador de empleado, agregandole los metodos de create, update y representation de forma masiva

--- bookstoreApp/serializers/employeeSerializer.py
+++ bookstoreApp/serializers/employeeSerializer.py
@@ -3,68 +3,62 @@ from bookstoreApp.models.user import User
 from bookstoreApp.models.employee import Employee
 from bookstoreApp.serializers.userSerializer import UserSerializer
 
-"""
-def employee_object(item):
-    print(item)  
-    userData = item.pop('user')
-    userInstance = User.objects.create_user(**userData)
-    employeeInstance= Employee(user= userInstance,**item) 
-    return employeeInstance
 
-class EmployeeListSerializer(serializers.ListSerializer): 
-    
+class EmployeeListSerializer(serializers.ListSerializer):
 
-    def create(self, validated_data):                
-        employees= [employee_object(item) for item in validated_data ]
+    def create(self, validated_data): 
+        employees=[]
+        for item in validated_data:
+            user_data = item.pop('user')
+            user_instance = User.objects.create(**user_data)
+            employees.append(Employee(user= user_instance,**item))   
         return Employee.objects.bulk_create(employees)
- 
+
     def update(self, instance, validated_data):
-        employee_mapping = {employee.id: employee for employee in instance}
-        data_mapping = {item['user_id']: item for item in validated_data}
-        ret = []
-        
-        for employee_id, data in data_mapping.items():
-            employee = employee_mapping.get(employee_id, None)
-            if employee is None:
-                ret.append(self.child.create(data))
+        employee_mapping = {employee.user_id: employee for employee in instance}
+        data_mapping = {item['user']['id']: item for item in validated_data}          
+        ret = []                      
+        for employee_id, data in data_mapping.items():          
+            user_data = data.pop('user', None)         
+            employee = employee_mapping.get(employee_id, None)                    
+            if employee is None:               
+                new_user= User.objects.create(**user_data)
+                ret.append(self.child.create(user= new_user, **data))
             else:
-                ret.append(self.child.update(employee, data))
+                User.objects.filter(id=employee_id).update(**user_data)
+                ret.append(self.child.update(employee, **data))
         
         for employee_id, employee in employee_mapping.items():
             if employee_id not in data_mapping:
-                employee.delete()          
+                User.objects.get(id=employee_id).delete()
+                #employee.delete()          
         return ret
-""" 
+
+    def to_representation(self, instance):
+        employee_representations= []
+        for employee in instance:
+            employee_representations.append({   
+                'id':employee.user_id,
+                'username': employee.user.username,
+                'fullname': employee.user.fullname,
+                'datebirth': employee.user.datebirth,
+                'email': employee.user.email,
+                'identification': employee.user.identification,
+                'phone number': employee.user.phone_number,
+                'address': employee.user.address,
+                'work area': employee.work_area.name,
+                'salary': employee.salary,
+                'is seller': employee.is_seller,
+                'is inventory manager': employee.is_inventory_manager,
+                'is admin': employee.is_admin
+                }) 
+        return employee_representations
 
 class EmployeeSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
+    user = UserSerializer() 
     class Meta:
-        model = Employee
+        model= Employee
         fields = '__all__'
         list_serializer_class = EmployeeListSerializer
-        
-    def create(self, validated_data):       
-        userData = validated_data.pop('user')
-        userInstance = User.objects.create_user(**userData)    
-        return Employee.objects.create(user= userInstance,**validated_data)
-        
-     
-    def to_representation(self, obj):
-        employee = obj
-        user = obj.user
-        return {
-            'id':user.id,
-            'username': user.username,
-            'fullname': user.fullname,
-            'datebirth': user.datebirth,
-            'email': user.email,
-            'identification': user.identification,
-            'phone number': user.phone_number,
-            'address': user.address,
-            'work area': employee.work_area.name,
-            'salary': employee.salary,
-            'is seller': employee.is_seller,
-            'is inventory manager': employee.is_inventory_manager,
-            'is admin': employee.is_admin
-        }
+
         
