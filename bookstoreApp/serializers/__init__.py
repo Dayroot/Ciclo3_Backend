ecb85584c7700fc58ec9627686debdf5d5b2c0bd
Dayroot <dallancaceresp@gmail.com>
2021-10-05 21:35:52 -0500

    Implemente el serializador para el modelo WorkArea

--- bookstoreApp/serializers/__init__.py
+++ bookstoreApp/serializers/__init__.py
@@ -5,3 +5,4 @@ from .bookSerializer import BookSerializer
 from .magazineSerializer import MagazineSerializer
 from .saleSerializer import SaleSerializer
 from .reservationSerializer import reservationSerializer
+from .workAreaSerializer import WorkAreaSerializer
