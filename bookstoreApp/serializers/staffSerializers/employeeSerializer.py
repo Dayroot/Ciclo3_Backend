16d7c77eefb8dd988b2a78f5d380cfd3474dead5
Dayroot <dallancaceresp@gmail.com>
2021-10-10 20:47:54 -0500

    Modifique el serializador de empleado, lo cambie por el serializador base de producto el cual se ajusta a la funcionalidad del serializador de empleado

--- bookstoreApp/serializers/staffSerializers/employeeSerializer.py
+++ bookstoreApp/serializers/staffSerializers/employeeSerializer.py
@@ -1,64 +1,21 @@
-#Django rest framework
-from rest_framework import serializers
-
 #Models
-from bookstoreApp.models import User
-from bookstoreApp.models import Employee
+from bookstoreApp.models import Employee, User
 
 #Serializers
+from .. import BaseProductSerializer, BaseProductListSerializer
 from . import UserSerializer,UserUpdateSerializer
 
-
-
-class EmployeeUpdateListSerializer(serializers.ListSerializer):
-    
-    def update(self, employee_mapping, validated_data):
-        
-        data_mapping = {item['user']['id'] : item for item in validated_data}          
-        ret = []                      
-        for id, data in data_mapping.items():          
-            user_data = data.pop('user', None)            
-            employee = employee_mapping.get(id, None)                    
-            User.objects.filter(id=id).update(**user_data)
-            ret.append(employee.update(**data))                        
-        return ret
-
-class EmployeeListSerializer(serializers.ListSerializer):
-    
-    def create(self, validated_data): 
-        employees=[]
-        for item in validated_data:
-            user_data = item.pop('user')
-            user_instance = User.objects.create(**user_data)
-            employees.append(Employee(user= user_instance,**item))   
-        return Employee.objects.bulk_create(employees)
-
-    def to_representation(self, instance):
-        employee_representations= []
-        for employee in instance:
-            employee_representations.append(self.child.to_representation(employee)) 
-        return employee_representations
-
-class EmployeeSerializer(serializers.ModelSerializer): 
+class EmployeeSerializer(BaseProductSerializer): 
 
     user= UserSerializer()
     class Meta:
         model= Employee
         fields = '__all__'
-        list_serializer_class = EmployeeListSerializer
-
+        list_serializer_class = BaseProductListSerializer
+    
     def create(self, validated_data):        
         user_data = validated_data.pop('user')
         user_instance = User.objects.create(**user_data)
         return  Employee.objects.create(user_id= user_instance.id,**validated_data)
-    
-    
-class EmployeeUpdateSerializer(serializers.ModelSerializer): 
+class EmployeeUpdateSerializer(EmployeeSerializer): 
     user= UserUpdateSerializer()
-    class Meta:
-        model= Employee
-        fields = '__all__'
-        list_serializer_class = EmployeeUpdateListSerializer
-
-
-    
