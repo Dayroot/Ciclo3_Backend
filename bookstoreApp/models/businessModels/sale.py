a06026ae44a1f8a9e48d8ebf1ff5364da5b6b7a4
Dayroot <dallancaceresp@gmail.com>
2021-10-10 20:26:51 -0500

    le agregue un parametro extra a producto, llamado description

--- bookstoreApp/models/businessModels/sale.py
+++ bookstoreApp/models/businessModels/sale.py
@@ -1,10 +1,8 @@
 from django.db import models
 from .. import Product
 from .. import User
-
-
 class Sale(models.Model):
     product = models.ForeignKey(Product, related_name='sold_products', on_delete=models.CASCADE)
-    user = models.ForeignKey(User, related_name='purchases', on_delete=models.CASCADE)
     quantity = models.IntegerField(default=0)
+    client = models.ForeignKey(User, related_name='purchases', on_delete=models.CASCADE)
     datetime= models.DateTimeField(auto_now=True)
