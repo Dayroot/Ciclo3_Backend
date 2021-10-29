058a73e174a6a1b97e347651b116ae45ac72a023
Dayroot <dallancaceresp@gmail.com>
2021-10-28 19:42:29 -0500

    le agregue el cifrado a la contraseña cuando se actualiza

--- bookstoreApp/views/userViews/userUpdateView.py
+++ bookstoreApp/views/userViews/userUpdateView.py
@@ -1,6 +1,6 @@
 #Django
 from django.conf import settings
-
+from django.contrib.auth.hashers import make_password
 #Django Rest framework
 from rest_framework import views, status
 from rest_framework.response import Response
@@ -23,8 +23,12 @@ class UserUpdateView(views.APIView):
         if valid_data['user_id'] != kwargs['pk']:
             stringResponse = {'detail':'Unauthorized Request'}
             return Response(stringResponse, status=status.HTTP_401_UNAUTHORIZED)
-        
+        data = request.data
+        if data.get("password") != None:
+            some_salt= 'mMUj0DrIK6vgtdIYepkIxN'
+            data['password'] = make_password(data['password'], some_salt)
+            
         instance = User.objects.filter(id= kwargs['pk'])
-        instance.update(**request.data)
+        instance.update(**data)
         return Response({'message': 'successful update'}, status= status.HTTP_200_OK)
     
